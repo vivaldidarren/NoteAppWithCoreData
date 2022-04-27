83c40bb0862c9aace05d2478bfcfe3720c984994
Vivaldi Darren Christophlus <vivdc2001@gmail.com>
2022-04-27 09:08:02 +0700

    Initial Commit

---
+++ NoteAppWithCoreData/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  NoteAppWithCoreData
+//
+//  Created by Vivaldi Darren Christophilus on 27/04/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
